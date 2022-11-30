947a76626db81e5295dfdd4576bb2569620d7ffb
Melchior Lozé <melchior.loze@epita.fr>
2022-11-30 11:55:32 +0100

    feat: init horizontal scroll

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,33 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const products = [
+    { name: '1' },
+    { name: '2' },
+    { name: '3' },
+    { name: '4' },
+    { name: '5' },
+    { name: '6' },
+    { name: '7' },
+    { name: '8' },
+    { name: '9' },
+    { name: '10' },
+    { name: '11' },
+    { name: '12' },
+    { name: '13' },
+    { name: '14' },
+    { name: '15' },
+    { name: '16' }
+  ];
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+    <div className="h-screen w-screen whitespace-nowrap overflow-x-scroll">
+      <div className="h-full w-full inline-block bg-red-500">test</div>
+      {products.map((product, index) => (
+        <div key={index} className="h-full w-1/3 inline-block bg-blue-500">
+          {product.name}
+        </div>
+      ))}
     </div>
   );
 }
