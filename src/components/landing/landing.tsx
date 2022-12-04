1fd20cdc73507bb2640b459f68ffedbbfcdd39c0
Tony Heng <tony.heng@epita.fr>
2022-12-04 15:52:30 +0100

    feat: display welcome to the black market

--- src/components/landing/landing.tsx
+++ src/components/landing/landing.tsx
@@ -3,10 +3,15 @@ import React from 'react';
 const Landing: React.FC = () => {
   return (
     <div className="flex h-full w-full justify-center items-center uppercase text-7xl relative">
-      dolor sit amet
+      <div className="uppercase flex flex-col font-bold justify-center items-center">
+        <span className="text-base">Welcome to the</span>
+        <h1>
+          <span className="text-neon">Black</span> <span className="text-neon-delay">Market</span>
+        </h1>
+      </div>
       <div className="absolute right-16 top-1/2 flex flex-col justify-center items-center gap-2 animate-pulse">
         <img src="/icons/icon_right.svg" alt="scroll right" className="w-16" />
-        <span className="text-2xl font-bold">SCROLL</span>
+        <span className="uppercase text-2xl font-bold">Scroll</span>
       </div>
     </div>
   );
